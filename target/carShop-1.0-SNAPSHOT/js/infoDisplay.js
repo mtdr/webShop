a9173be1f5854f22b824969380377ef9718371b3
Mtdr <paul.osmolovsky@gmail.com>
2016-10-24 13:48:35 +0300

    0.0.0
    initialize commit

---
+++ target/carShop-1.0-SNAPSHOT/js/infoDisplay.js
@@ -0,0 +1,26 @@
+function show(par) {
+    var elem = document.getElementsByClassName('nav-li');
+    for (i = 0; i < elem.length; i++) {
+        elem[i].style.backgroundColor = '#FF0000';
+    }
+    var elem1 = document.getElementsByClassName('tabs');
+    for (i = 0; i < elem1.length; i++) {
+        elem1[i].style.display = 'none';
+    }
+    console.log(par);
+    switch (par) {
+        case '1':
+            document.getElementById('short').style.display='block';
+            document.getElementById('li1').style.backgroundColor='#8B0000';
+            break;
+        case '2':
+            document.getElementById('detail').style.display='block';
+            document.getElementById('li2').style.backgroundColor='#8B0000';
+            break;
+        case '3':
+            document.getElementById('reviews').style.display='block';
+            document.getElementById('li3').style.backgroundColor='#8B0000';
+            break;
+    }
+}
+
